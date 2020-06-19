4d0a90014bbc924f7f8f9100e8a35628da8137c7
sunnysab <sunnysab@yeah.net>
2020-06-19 19:54:38 +0800

    首次提交项目文件, 包含一小段测试代码和抓取的网页

---
+++ html/README.md
@@ -0,0 +1,9 @@
+
+
+
+| 编号 | 文件名                | 文件描述      | 链接                                                         |
+| ---- | --------------------- | ------------- | ------------------------------------------------------------ |
+| 1    | 第二课堂最近活动.html | 最近200个活动 | [Link](http://sc.sit.edu.cn/public/activity/activityList.action?pageNo=1&pageSize=200&categoryId=&activityName=) |
+| 2    | 第二课堂得分页面.html | 页面有pageSize，pageNo 参数  |[Link](http://sc.sit.edu.cn/public/pcenter/scoreDetail.action?pageSize=200) |
+|      |                       |               |                                                              |
+
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use scraper::{Html, Selector};
+use std::fs::File;
+use std::io::Read;
+
+fn recent_activity() {
+    let html_page = std::fs::read_to_string("html\\第二课堂最近活动.html").unwrap();
+    let document = Html::parse_document(html_page.as_ref());
+    let selector = Selector::parse(".ul_7 li>a").unwrap();
+
+    for element in document.select(&selector) {
+        println!("活动标题 {}, 链接 {}", element.inner_html(), element.value().attr("href").unwrap());
+    }
+}
+
+
+fn my_score() {
+    let html_page = std::fs::read_to_string("html\\第二课堂得分页面.html").unwrap();
+    let document = Html::parse_document(html_page.as_ref());
+    let selector = Selector::parse("table[width=\"100%\"]>tbody>tr").unwrap();
+
+    // <td>如何防范校园贷</td>
+    // <td> <a title="-&quot;社团社区易班、学院活动&quot;"> 社团社区易班、...  </a> </td>
+    // <td>100887790 </td>
+    // <td>2018-9-26 18:57:12</td>
+    // <td> <span style="color:green">+0.1</span> </td>
+    // <td> <span style="color:green">+0.2</span> </td>
+    // <td style="color:red"></td>
+    for element in document.select(&selector) {
+        let mut all_columns = element.children();
+        println!("{}", all_columns.nth(0).unwrap().value().as_text().unwrap().text);
+    }
+}
+
+fn main() {
+    my_score();
+}
